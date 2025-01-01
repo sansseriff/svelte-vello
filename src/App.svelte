d4047dcee6d82f0924285fa0ab8f1a5c2b888d6b
Andrew Mueller <amueller@caltech.edu>
2024-12-31 18:19:00 -0800

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import svelteLogo from "./assets/svelte.svg";
+  import viteLogo from "/vite.svg";
+  import Counter from "./lib/Counter.svelte";
+  import { greet } from "../pkg/svelte_vello";
+</script>
+
+<main>
+  <h2>{greet("The Friendly")}</h2>
+</main>
+
+<style>
+</style>
